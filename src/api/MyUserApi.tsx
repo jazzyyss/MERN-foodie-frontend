d45773b9dbf943c0ddd3833c11cb149347261236
Jaspreet Singh <jaspreetbrar1636@gmail.com>
2024-07-26 16:28:43 -0400

    final commit

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -3,116 +3,126 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
-const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useGetMyUser = () => {
-    const { getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
-    const getMyUserRequest = async (): Promise<User> => {
-        const accessToken = await getAccessTokenSilently();
+  const getMyUserRequest = async (): Promise<User> => {
+    const accessToken = await getAccessTokenSilently();
 
-        const response = await fetch(`${API_BASE_URL}/api/my/user`,{
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/json"
-            }
-        });
-        if(!response.ok){
-            throw new Error("Failed to fetch user")
-        }
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-        return response.json();
+    if (!response.ok) {
+      throw new Error("Failed to fetch user");
     }
 
-    const { data: currentUser, isLoading, error } = useQuery("fetchCurrentUser", getMyUserRequest);
+    return response.json();
+  };
 
-    if(error){
-        toast.error(error.toString());
-    }
-    return { currentUser, isLoading }
-}
+  const {
+    data: currentUser,
+    isLoading,
+    error,
+  } = useQuery("fetchCurrentUser", getMyUserRequest);
+
+  if (error) {
+    toast.error(error.toString());
+  }
+
+  return { currentUser, isLoading };
+};
 
 type CreateUserRequest = {
-    auth0Id: string;
-    email: string;
-}
+  auth0Id: string;
+  email: string;
+};
 
 export const useCreateMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
 
-    const { getAccessTokenSilently } = useAuth0();
+  const createMyUserRequest = async (user: CreateUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
 
-    const createMyUserRequest = async(user: CreateUserRequest) => {
-        const accessToken = await getAccessTokenSilently();
-        const response = await fetch(`${API_BASE_URL}/api/my/user`,{
-            method: "POST",
-            headers:{
-                Authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(user)
-        });
+    if (!response.ok) {
+      throw new Error("Failed to create user");
+    }
+  };
 
-        if(!response.ok){
-            throw new Error("Failed to create user.")
-        }
-    };
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest);
 
-    const {mutateAsync: createUser, isLoading, isError, isSuccess} = useMutation(createMyUserRequest);
+  return {
+    createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  };
+};
 
-    return {
-        createUser, 
-        isLoading,
-        isError,
-        isSuccess
-    };
-}
-
-type updateMyUserRequest = {
-    name: string;
-    addressLine1: string;
-    city: string;
-    country: string;
-}
+type UpdateMyUserRequest = {
+  name: string;
+  addressLine1: string;
+  city: string;
+  country: string;
+};
 
 export const useUpdateMyUser = () => {
-    const { getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
-    const updateMyUserRequest = async (formData: updateMyUserRequest) => {
-        const accessToken = await getAccessTokenSilently();
-        const response = await fetch(`${API_BASE_URL}/api/my/user`,{
-            method: "PUT",
-            headers: {
-                authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData)
-        });
-        if(!response.ok){
-            throw new Error("Failed to update user")
-        }
-        return response.json();
-    };
+  const updateMyUserRequest = async (formData: UpdateMyUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
 
-    const { 
-        mutateAsync: updateUser, 
-        isLoading, 
-        isSuccess, 
-        error, 
-        reset
-     } = useMutation(updateMyUserRequest);
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
 
-     if(isSuccess){
-        toast.success("User profile updated!")
-     }
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
 
-     if(error){
-        toast.error(error.toString());
-        reset();
-     }
+    return response.json();
+  };
 
-     return {
-        updateUser, 
-        isLoading
-    };
+  const {
+    mutateAsync: updateUser,
+    isLoading,
+    isSuccess,
+    error,
+    reset,
+  } = useMutation(updateMyUserRequest);
+
+  if (isSuccess) {
+    toast.success("User profile updated!");
+  }
+
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
+
+  return { updateUser, isLoading };
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './global.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import AppRoutes from './app-routes'
-import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Toaster } from './components/ui/sonner'
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./app-routes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient({
-  defaultOptions:{
-    queries:{
-      refetchOnWindowFocus: false
-    }
-  }
-})
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <AppRoutes />
-          <Toaster visibleToasts={1} position="top-right" richColors/>
+          <Toaster visibleToasts={1} position="top-right" richColors />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
